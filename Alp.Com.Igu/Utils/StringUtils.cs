1108e103352e0ff8dc70de5d530d5144fe6699d6
a-cris-75 <159661657+a-cris-75@users.noreply.github.com>
2025-08-22 09:07:22 +0200

    Add files via upload

---
+++ Alp.Com.Igu/Utils/StringUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alp.Com.Igu.Utils
+{
+    public static class StringUtils
+    {
+        public static string JoinFilter(string separator, IEnumerable<string> strings)
+        {
+            return string.Join(separator, strings.Where(s => !string.IsNullOrEmpty(s)));
+        }
+        public static string JoinFilter(string separator, params string[] str)
+        {
+            return string.Join(separator, str?.Where(s => !string.IsNullOrEmpty(s)));
+        }
+    }
+}
