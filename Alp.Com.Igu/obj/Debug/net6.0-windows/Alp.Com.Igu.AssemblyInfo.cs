759d94d712ebf67407edbf515b9ad27825549ba7
Cristian Azzolin <c.azzolin@alping.it>
2025-08-28 18:44:04 +0200

    Debug in impianto

--- Alp.Com.Igu/obj/Debug/net6.0-windows/Alp.Com.Igu.AssemblyInfo.cs
+++ Alp.Com.Igu/obj/Debug/net6.0-windows/Alp.Com.Igu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alp.Com.Igu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eb3ac9e7f290e7362624c76099bb7fea6a45446")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31a6bfdd3406fea8e6925f35e861de39a9ffba28")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alp.Com.Igu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alp.Com.Igu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
